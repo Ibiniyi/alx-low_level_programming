270fc6e76475e3b4e6ba3a513722827062e312e0
Ibiniyi <timothyniyi@gmail.com>
2022-10-13 07:12:46 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - printing
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
